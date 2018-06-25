e3337a70cb2b17afad72cdfac675867fb8e6632c
Laura Bodell <bodelllaura@gmail.com>
2018-06-25 21:07:23 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,16 +15,13 @@ class StudentsController < ApplicationController
       @student = Student.find(params[:id])
     end
     
-    def activate 
-    @student = Student.find(params[:id])
-    if @student.active 
-    @student.active = false
-  else
-     @student.active = true
-    end 
+   def activate
+ @student = Student.find(params[:id])
+@student.active = !@student.active
     @student.save
-    
-    end 
+    redirect_to student_path(@student)
+   end
+ 
     
 
     
