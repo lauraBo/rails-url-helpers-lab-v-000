f3bab04fa222a059f72a012b2c18e7c5bed4b7c5
Laura Bodell <bodelllaura@gmail.com>
2018-06-25 21:02:03 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
  
   get "/students", to: "students#index"
   get "/students/:id", to: "students#show" 
-  get "/students/:id/activate", to: "students#activate", as: 'activate_student'
+  get "/students/:id/activate", to: "students#activate", as: "activate_student"
 end
