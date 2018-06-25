845bf91854fb0a836d0a0cbd0fe808c9ce37c78f
Laura Bodell <bodelllaura@gmail.com>
2018-06-25 21:06:00 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -23,7 +23,7 @@ class StudentsController < ApplicationController
      @student.active = true
     end 
     @student.save
-    redirect_to student_path(@student)
+    redirect_to erb: :show 
     end 
     
 
